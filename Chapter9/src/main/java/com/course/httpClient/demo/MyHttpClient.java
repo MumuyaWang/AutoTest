3d74589012781313994ba97ff2fe0ad8f79d24c7
Administrator <chu_er2@163.c0m>
2019-01-21 23:29:30 +0800

    学习提交

---
+++ Chapter9/src/main/java/com/course/httpClient/demo/MyHttpClient.java
@@ -0,0 +1,27 @@
+package com.course.httpClient.demo;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.testng.annotations.Test;
+import java.io.IOException;
+
+public class MyHttpClient {
+
+    @Test
+    public void test1() throws IOException {
+        //用来存放结果
+        String result;
+        HttpGet get = new HttpGet("http://www.baidu.com");
+        //这是用来执行get方法的
+        //HttpClient client = new DefaultHttpClient();      //过时
+        HttpClient client = HttpClients.createDefault();
+        client.execute(get);
+        HttpResponse response = client.execute(get);
+        result =EntityUtils.toString(response.getEntity(),"utf-8");
+        System.out.println(result);
+    }
+}
