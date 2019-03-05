dba7883b4223251891fb64dca3e002b166de3435
Wangchu <wangchu@dianwoba.com>
2019-03-05 17:32:14 +0800

    提交学习

--- Chapter10/src/main/java/Application.java
+++ Chapter10/src/main/java/Application.java
@@ -2,11 +2,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-@ComponentScan("com.course.server")
+@SpringBootApplication                 //
+@ComponentScan("com.course.server")    //需要扫描的包
+//入口类
 public class Application {
-
     public static void main(String[] args){
-        SpringApplication.run(Application.class,args);
+        SpringApplication.run(Application.class,args);    //args就是个参数
     }
 }
--- Chapter10/src/main/java/com/course/server/MyGetMethod.java
+++ Chapter10/src/main/java/com/course/server/MyGetMethod.java
@@ -4,11 +4,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
+
+@RestController   //我是需要被扫描的类
 public class MyGetMethod {
-    @RequestMapping(value="/getCookies",method = RequestMethod.GET)     //访问路径
-    public String getCookies(){
+    @RequestMapping(value="/getCookies",method = RequestMethod.GET)     //访问路径，使用RequestMethod.GET这种方法访问
+    public String getCookies(HttpServletResponse response){
+        //HttpServerLetRequest装请求信息的类
+        //HttpServerLetResponse装响应信息的类
+        Cookie cookie = new Cookie("login","true");
+        response.addCookie(cookie);     //添加cookie
         return "恭喜你获取cookies成功";
     }
-
 }
