f0d5f090709f4e9c961e82d71969010b514827c9
Wangchu <wangchu@dianwoba.com>
2019-03-05 21:42:47 +0800

    提交学习

--- Chapter10/src/main/java/com/course/server/MyGetMethod.java
+++ Chapter10/src/main/java/com/course/server/MyGetMethod.java
@@ -5,11 +5,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Objects;
 
 
 @RestController   //我是需要被扫描的类
 public class MyGetMethod {
+    /**
+     * 10-3 返回cookies信息的get接口开发
+     */
     @RequestMapping(value="/getCookies",method = RequestMethod.GET)     //访问路径，使用RequestMethod.GET这种方法访问
     public String getCookies(HttpServletResponse response){
         //HttpServerLetRequest装请求信息的类
@@ -17,5 +22,24 @@ public class MyGetMethod {
         Cookie cookie = new Cookie("login","true");
         response.addCookie(cookie);     //添加cookie
         return "恭喜你获取cookies成功";
+    }
+
+    /**
+     * 10-4 一个要求携带cookies信息访问的get接口开发
+     * 要求客户端携带cookies访问
+     * 这是一个需要携带cookies信息访问的get接口开发
+     * */
+    @RequestMapping(value="/get/with/cookies",method = RequestMethod.GET)
+    public String getWithCookies(HttpServletRequest request){
+        Cookie[] cookies =request.getCookies();
+        if(Objects.isNull(cookies)){
+            return "你必须携带cookies信息来";
+        }
+        for(Cookie cookie: cookies){
+            if (cookie.getName().equals("login") && cookie.getValue().equals("true")){
+                return "恭喜你访问成功";
+            }
+        }
+        return "你必须携带cookies信息来";
     }
 }
