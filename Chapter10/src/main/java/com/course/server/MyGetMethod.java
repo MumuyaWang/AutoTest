d13f2b40987999aa9b6b1514ff8a3771250031ac
Wangchu <wangchu@dianwoba.com>
2019-03-06 22:00:10 +0800

    提交学习

--- Chapter10/src/main/java/com/course/server/MyGetMethod.java
+++ Chapter10/src/main/java/com/course/server/MyGetMethod.java
@@ -2,11 +2,14 @@ package com.course.server;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 
@@ -37,9 +40,37 @@ public class MyGetMethod {
         }
         for(Cookie cookie: cookies){
             if (cookie.getName().equals("login") && cookie.getValue().equals("true")){
-                return "恭喜你访问成功";
+                return "这是一个需要携带cookies信息才能访问的get请求";
             }
         }
         return "你必须携带cookies信息来";
     }
+
+    /**
+     * 开发一个需要携带参数才能访问的get请求
+     * 第一种实现方式  url : key = value & key = value
+     * 我们来模拟获取商品列表
+     * */
+    @RequestMapping(value ="/get/with/param",method =RequestMethod.GET )
+    public Map<String,Integer> getList(@RequestParam Integer start, @RequestParam Integer end){
+        Map<String ,Integer> myList = new HashMap<>();
+        myList.put("鞋",400);
+        myList.put("干脆面",1);
+        myList.put("衬衫",200);
+        return myList;
+    }
+
+    /**
+     * 第二种需要携带参数访问的get请求
+     * url:ip:port/get/with/param/10/20
+     * */
+    @RequestMapping(value ="/get/with/param/{start}/{end}" )
+    public Map<String,Integer> mygetList(@RequestParam Integer start, @RequestParam Integer end){
+        Map<String ,Integer> myList = new HashMap<>();
+        myList.put("鞋",400);
+        myList.put("干脆面",1);
+        myList.put("衬衫",200);
+        return myList;
+    }
+
 }
